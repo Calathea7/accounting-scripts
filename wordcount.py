defd7b18347826ffaca8b1438a335f3a8a1c623c
Anastasia Correa <correa.anastasia7@gmail.com>
2020-07-13 19:58:49 +0000

    Wordcount

---
+++ wordcount.py
@@ -0,0 +1,26 @@
+# put your code here.
+
+import sys
+filename = sys.argv[1]
+
+def count_words(filename):
+
+    all_data = open(filename)
+
+    word_count = {}
+
+    for line in all_data:
+
+        words = line.strip().split(" ")
+
+        for word in words:
+            word = word.lower().strip('._,?!;:"')
+            word_count[word] = word_count.get(word, 0) + 1
+
+    for word, count in word_count.items():
+        print(str(word) + " " + str(count))
+
+count_words(filename)
+        
+
+ 
